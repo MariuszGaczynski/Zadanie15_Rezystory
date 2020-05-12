07d46bbdeca4d0262aaa5bcbb79f23f26fb7b02d
Mariusz Gaczyński <kontakt@mariuszgaczynski.com>
2020-05-12 16:56:48 +0200

    Add project files.

---
+++ Zadanie15_Rezystory/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace Zadanie15_Rezystory
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboFirstBand = new System.Windows.Forms.ComboBox();
+            this.pnlFirstBand = new System.Windows.Forms.Panel();
+            this.pnlSecondBand = new System.Windows.Forms.Panel();
+            this.pnlThirdBand = new System.Windows.Forms.Panel();
+            this.pnlFourthBand = new System.Windows.Forms.Panel();
+            this.lblResistanceValue = new System.Windows.Forms.Label();
+            this.comboSecondBand = new System.Windows.Forms.ComboBox();
+            this.comboThirdBand = new System.Windows.Forms.ComboBox();
+            this.comboFourthBand = new System.Windows.Forms.ComboBox();
+            this.lblFirstBand = new System.Windows.Forms.Label();
+            this.lblSecondBand = new System.Windows.Forms.Label();
+            this.lblThirdBand = new System.Windows.Forms.Label();
+            this.lblFourthBand = new System.Windows.Forms.Label();
+            this.btnProceed = new System.Windows.Forms.Button();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboFirstBand
+            // 
+            this.comboFirstBand.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.comboFirstBand.FormattingEnabled = true;
+            this.comboFirstBand.Items.AddRange(new object[] {
+            "Black",
+            "Brown",
+            "Red",
+            "Orange",
+            "Yellow",
+            "Green",
+            "Blue",
+            "Violet",
+            "Gray",
+            "White"});
+            this.comboFirstBand.Location = new System.Drawing.Point(12, 15);
+            this.comboFirstBand.Name = "comboFirstBand";
+            this.comboFirstBand.Size = new System.Drawing.Size(146, 28);
+            this.comboFirstBand.TabIndex = 0;
+            this.comboFirstBand.SelectedIndexChanged += new System.EventHandler(this.comboFirstBand_SelectedIndexChanged);
+            // 
+            // pnlFirstBand
+            // 
+            this.pnlFirstBand.BackColor = System.Drawing.Color.White;
+            this.pnlFirstBand.Location = new System.Drawing.Point(12, 265);
+            this.pnlFirstBand.Name = "pnlFirstBand";
+            this.pnlFirstBand.Size = new System.Drawing.Size(40, 229);
+            this.pnlFirstBand.TabIndex = 1;
+            // 
+            // pnlSecondBand
+            // 
+            this.pnlSecondBand.BackColor = System.Drawing.Color.White;
+            this.pnlSecondBand.Location = new System.Drawing.Point(74, 265);
+            this.pnlSecondBand.Name = "pnlSecondBand";
+            this.pnlSecondBand.Size = new System.Drawing.Size(40, 229);
+            this.pnlSecondBand.TabIndex = 2;
+            // 
+            // pnlThirdBand
+            // 
+            this.pnlThirdBand.BackColor = System.Drawing.Color.White;
+            this.pnlThirdBand.Location = new System.Drawing.Point(171, 265);
+            this.pnlThirdBand.Name = "pnlThirdBand";
+            this.pnlThirdBand.Size = new System.Drawing.Size(40, 229);
+            this.pnlThirdBand.TabIndex = 3;
+            // 
+            // pnlFourthBand
+            // 
+            this.pnlFourthBand.BackColor = System.Drawing.Color.White;
+            this.pnlFourthBand.Location = new System.Drawing.Point(298, 265);
+            this.pnlFourthBand.Name = "pnlFourthBand";
+            this.pnlFourthBand.Size = new System.Drawing.Size(40, 229);
+            this.pnlFourthBand.TabIndex = 4;
+            // 
+            // lblResistanceValue
+            // 
+            this.lblResistanceValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblResistanceValue.Location = new System.Drawing.Point(12, 199);
+            this.lblResistanceValue.Name = "lblResistanceValue";
+            this.lblResistanceValue.Size = new System.Drawing.Size(326, 51);
+            this.lblResistanceValue.TabIndex = 5;
+            this.lblResistanceValue.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // comboSecondBand
+            // 
+            this.comboSecondBand.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.comboSecondBand.FormattingEnabled = true;
+            this.comboSecondBand.Items.AddRange(new object[] {
+            "Black",
+            "Brown",
+            "Red",
+            "Orange",
+            "Yellow",
+            "Green",
+            "Blue",
+            "Violet",
+            "Gray",
+            "White"});
+            this.comboSecondBand.Location = new System.Drawing.Point(12, 49);
+            this.comboSecondBand.Name = "comboSecondBand";
+            this.comboSecondBand.Size = new System.Drawing.Size(146, 28);
+            this.comboSecondBand.TabIndex = 6;
+            this.comboSecondBand.SelectedIndexChanged += new System.EventHandler(this.comboSecondBand_SelectedIndexChanged);
+            // 
+            // comboThirdBand
+            // 
+            this.comboThirdBand.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.comboThirdBand.FormattingEnabled = true;
+            this.comboThirdBand.Items.AddRange(new object[] {
+            "Pink",
+            "Silver",
+            "Gold",
+            "Black",
+            "Brown",
+            "Red",
+            "Orange",
+            "Yellow",
+            "Green",
+            "Blue",
+            "Violet",
+            "Gray",
+            "White"});
+            this.comboThirdBand.Location = new System.Drawing.Point(12, 83);
+            this.comboThirdBand.Name = "comboThirdBand";
+            this.comboThirdBand.Size = new System.Drawing.Size(146, 28);
+            this.comboThirdBand.TabIndex = 7;
+            this.comboThirdBand.SelectedIndexChanged += new System.EventHandler(this.comboThirdBand_SelectedIndexChanged);
+            // 
+            // comboFourthBand
+            // 
+            this.comboFourthBand.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.comboFourthBand.FormattingEnabled = true;
+            this.comboFourthBand.Items.AddRange(new object[] {
+            "None",
+            "Silver",
+            "Gold",
+            "Brown",
+            "Red",
+            "Orange",
+            "Yellow",
+            "Green",
+            "Blue",
+            "Violet",
+            "Gray"});
+            this.comboFourthBand.Location = new System.Drawing.Point(12, 117);
+            this.comboFourthBand.Name = "comboFourthBand";
+            this.comboFourthBand.Size = new System.Drawing.Size(146, 28);
+            this.comboFourthBand.TabIndex = 8;
+            this.comboFourthBand.SelectedIndexChanged += new System.EventHandler(this.comboFourthBand_SelectedIndexChanged);
+            // 
+            // lblFirstBand
+            // 
+            this.lblFirstBand.AutoSize = true;
+            this.lblFirstBand.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblFirstBand.Location = new System.Drawing.Point(180, 23);
+            this.lblFirstBand.Name = "lblFirstBand";
+            this.lblFirstBand.Size = new System.Drawing.Size(123, 20);
+            this.lblFirstBand.TabIndex = 9;
+            this.lblFirstBand.Text = "First Band Color";
+            // 
+            // lblSecondBand
+            // 
+            this.lblSecondBand.AutoSize = true;
+            this.lblSecondBand.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblSecondBand.Location = new System.Drawing.Point(180, 57);
+            this.lblSecondBand.Name = "lblSecondBand";
+            this.lblSecondBand.Size = new System.Drawing.Size(147, 20);
+            this.lblSecondBand.TabIndex = 10;
+            this.lblSecondBand.Text = "Second Band Color";
+            // 
+            // lblThirdBand
+            // 
+            this.lblThirdBand.AutoSize = true;
+            this.lblThirdBand.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblThirdBand.Location = new System.Drawing.Point(180, 91);
+            this.lblThirdBand.Name = "lblThirdBand";
+            this.lblThirdBand.Size = new System.Drawing.Size(127, 20);
+            this.lblThirdBand.TabIndex = 11;
+            this.lblThirdBand.Text = "Third Band Color";
+            // 
+            // lblFourthBand
+            // 
+            this.lblFourthBand.AutoSize = true;
+            this.lblFourthBand.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblFourthBand.Location = new System.Drawing.Point(180, 125);
+            this.lblFourthBand.Name = "lblFourthBand";
+            this.lblFourthBand.Size = new System.Drawing.Size(139, 20);
+            this.lblFourthBand.TabIndex = 12;
+            this.lblFourthBand.Text = "Fourth Band Color";
+            // 
+            // btnProceed
+            // 
+            this.btnProceed.Enabled = false;
+            this.btnProceed.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnProceed.Location = new System.Drawing.Point(12, 160);
+            this.btnProceed.Name = "btnProceed";
+            this.btnProceed.Size = new System.Drawing.Size(326, 36);
+            this.btnProceed.TabIndex = 14;
+            this.btnProceed.Text = "Value of electrical resistivity of given resistor is:";
+            this.btnProceed.UseVisualStyleBackColor = true;
+            this.btnProceed.Click += new System.EventHandler(this.btnProceed_Click);
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Location = new System.Drawing.Point(12, 506);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(326, 23);
+            this.btnRestart.TabIndex = 16;
+            this.btnRestart.Text = "Restart";
+            this.btnRestart.UseVisualStyleBackColor = true;
+            this.btnRestart.Click += new System.EventHandler(this.btnRestart_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(351, 539);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.btnProceed);
+            this.Controls.Add(this.lblFourthBand);
+            this.Controls.Add(this.lblThirdBand);
+            this.Controls.Add(this.lblSecondBand);
+            this.Controls.Add(this.lblFirstBand);
+            this.Controls.Add(this.comboFourthBand);
+            this.Controls.Add(this.comboThirdBand);
+            this.Controls.Add(this.comboSecondBand);
+            this.Controls.Add(this.lblResistanceValue);
+            this.Controls.Add(this.pnlFourthBand);
+            this.Controls.Add(this.pnlThirdBand);
+            this.Controls.Add(this.pnlSecondBand);
+            this.Controls.Add(this.pnlFirstBand);
+            this.Controls.Add(this.comboFirstBand);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Resistivity Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboFirstBand;
+        private System.Windows.Forms.Panel pnlFirstBand;
+        private System.Windows.Forms.Panel pnlSecondBand;
+        private System.Windows.Forms.Panel pnlThirdBand;
+        private System.Windows.Forms.Panel pnlFourthBand;
+        private System.Windows.Forms.Label lblResistanceValue;
+        private System.Windows.Forms.ComboBox comboSecondBand;
+        private System.Windows.Forms.ComboBox comboThirdBand;
+        private System.Windows.Forms.ComboBox comboFourthBand;
+        private System.Windows.Forms.Label lblFirstBand;
+        private System.Windows.Forms.Label lblSecondBand;
+        private System.Windows.Forms.Label lblThirdBand;
+        private System.Windows.Forms.Label lblFourthBand;
+        private System.Windows.Forms.Button btnProceed;
+        private System.Windows.Forms.Button btnRestart;
+    }
+}
+
---
+++ Zadanie15_Rezystory/Form1.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadanie15_Rezystory
+{
+    public partial class MainForm : Form
+    {
+        byte firstNumber, secondNumber;
+        decimal multiplier, tolerance;
+        bool band1Set, band2Set, band3Set, band4Set = false;
+
+        
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+        }
+
+        
+
+        private void comboFirstBand_SelectedIndexChanged(object sender, EventArgs e)
+        { 
+                if (comboFirstBand.SelectedIndex > -1)
+                {
+                    //lblResistanceValue.Text = String.Format("Pozycja {0} - kolor {1}",
+                    //    comboFirstBand.SelectedIndex, comboFirstBand.SelectedItem);
+
+                    switch (comboFirstBand.SelectedIndex)
+                    {
+
+                        case 0:
+                            pnlFirstBand.BackColor = Color.Black;
+                            firstNumber = 0;
+                            break;
+                        case 1:
+                            pnlFirstBand.BackColor = Color.FromArgb(128, 58, 10);
+                            firstNumber = 1;
+                            break;
+                        case 2:
+                            pnlFirstBand.BackColor = Color.Red;
+                            firstNumber = 2;
+                            break;
+                        case 3:
+                            pnlFirstBand.BackColor = Color.DarkOrange;
+                            firstNumber = 3;
+                            break;
+                        case 4:
+                            pnlFirstBand.BackColor = Color.Yellow;
+                            firstNumber = 4;
+                            break;
+                        case 5:
+                            pnlFirstBand.BackColor = Color.Green;
+                            firstNumber = 5;
+                            break;
+                        case 6:
+                            pnlFirstBand.BackColor = Color.Blue;
+                            firstNumber = 6;
+                            break;
+                        case 7:
+                            pnlFirstBand.BackColor = Color.DarkViolet;
+                            firstNumber = 7;
+                            break;
+                        case 8:
+                            pnlFirstBand.BackColor = Color.LightGray;
+                            firstNumber = 8;
+                            break;
+                        case 9:
+                            pnlFirstBand.BackColor = Color.AntiqueWhite;
+                            firstNumber = 9;
+                            break;
+                        default:
+                                break;
+
+                    }
+                }
+
+            band1Set = true;
+            if (band1Set == true && band2Set == true &&
+                band3Set == true && band4Set == true)
+            {
+                btnProceed.Enabled = true;
+            }
+
+            lblResistanceValue.Text = String.Empty;
+
+        } // koniec kombo pierwszego paska
+
+        
+
+        private void comboSecondBand_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboSecondBand.SelectedIndex > -1)
+            {
+                
+
+                switch (comboSecondBand.SelectedIndex)
+                {
+                    
+                    case 0:
+                        pnlSecondBand.BackColor = Color.Black;
+                        secondNumber = 0;
+                        break;
+                    case 1:
+                        pnlSecondBand.BackColor = Color.FromArgb(128, 58, 10);
+                        secondNumber = 1;
+                        break;
+                    case 2:
+                        pnlSecondBand.BackColor = Color.Red;
+                        secondNumber = 2;
+                        break;
+                    case 3:
+                        pnlSecondBand.BackColor = Color.DarkOrange;
+                        secondNumber = 3;
+                        break;
+                    case 4:
+                        pnlSecondBand.BackColor = Color.Yellow;
+                        secondNumber = 4;
+                        break;
+                    case 5:
+                        pnlSecondBand.BackColor = Color.Green;
+                        secondNumber = 5;
+                        break;
+                    case 6:
+                        pnlSecondBand.BackColor = Color.Blue;
+                        secondNumber = 6;
+                        break;
+                    case 7:
+                        pnlSecondBand.BackColor = Color.DarkViolet;
+                        secondNumber = 7;
+                        break;
+                    case 8:
+                        pnlSecondBand.BackColor = Color.LightGray;
+                        secondNumber = 8;
+                        break;
+                    case 9:
+                        pnlSecondBand.BackColor = Color.AntiqueWhite;
+                        secondNumber = 9;
+                        break;
+                    default:
+                        break;
+
+                }
+            }
+
+            band2Set = true;
+            if (band1Set == true && band2Set == true &&
+                band3Set == true && band4Set == true)
+            {
+                btnProceed.Enabled = true;
+            }
+
+            lblResistanceValue.Text = String.Empty;
+
+        } // koniec kombo drugiego paska
+
+
+
+        private void comboThirdBand_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboThirdBand.SelectedIndex > -1)
+            {
+
+
+                switch (comboThirdBand.SelectedIndex)
+                {
+                    
+                    case 0:
+                        pnlThirdBand.BackColor = Color.Fuchsia;
+                        multiplier = 0.001m;
+                        break;
+                    case 1:
+                        pnlThirdBand.BackColor = Color.FromArgb(192, 192, 192);
+                        multiplier = 0.01m;
+                        break;
+                    case 2:
+                        pnlThirdBand.BackColor = Color.FromArgb(207, 181, 59);
+                        multiplier = 0.1m;
+                        break;
+                    case 3:
+                        pnlThirdBand.BackColor = Color.Black;
+                        multiplier = 1m;
+                        break;
+                    case 4:
+                        pnlThirdBand.BackColor = Color.FromArgb(128, 58, 10);
+                        multiplier = 10m;
+                        break;
+                    case 5:
+                        pnlThirdBand.BackColor = Color.Red;
+                        multiplier = 100m;
+                        break;
+                    case 6:
+                        pnlThirdBand.BackColor = Color.DarkOrange;
+                        multiplier = 1000m;
+                        break;
+                    case 7:
+                        pnlThirdBand.BackColor = Color.Yellow;
+                        multiplier = 10000m;
+                        break;
+                    case 8:
+                        pnlThirdBand.BackColor = Color.Green;
+                        multiplier = 100000m;
+                        break;
+                    case 9:
+                        pnlThirdBand.BackColor = Color.Blue;
+                        multiplier = 1000000m;
+                        break;
+                    case 10:
+                        pnlThirdBand.BackColor = Color.DarkViolet;
+                        multiplier = 10000000m;
+                        break;
+                    case 11:
+                        pnlThirdBand.BackColor = Color.LightGray;
+                        multiplier = 100000000m;
+                        break;
+                    case 12:
+                        pnlThirdBand.BackColor = Color.AntiqueWhite;
+                        multiplier = 1000000000m;
+                        break;
+                    default:
+                        break;
+
+                }
+            }
+
+            band3Set = true;
+            if (band1Set == true && band2Set == true &&
+                band3Set == true && band4Set == true)
+            {
+                btnProceed.Enabled = true;
+            }
+
+            lblResistanceValue.Text = String.Empty;
+
+
+        } // koniec kombo trzeciego paska
+
+
+        private void comboFourthBand_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboFourthBand.SelectedIndex > -1)
+            {
+
+
+                switch (comboFourthBand.SelectedIndex)
+                {
+                    
+                    case 0:
+                        pnlFourthBand.BackColor = Color.White;
+                        tolerance = 20m;
+                        break;
+                    case 1:
+                        pnlFourthBand.BackColor = Color.FromArgb(192, 192, 192);
+                        tolerance = 10m;
+                        break;
+                    case 2:
+                        pnlFourthBand.BackColor = Color.FromArgb(207, 181, 59);
+                        tolerance = 5m;
+                        break;
+                    case 3:
+                        pnlFourthBand.BackColor = Color.FromArgb(128, 58, 10);
+                        tolerance = 1m;
+                        break;
+                    case 4:
+                        pnlFourthBand.BackColor = Color.Red;
+                        tolerance = 2m;
+                        break;
+                    case 5:
+                        pnlFourthBand.BackColor = Color.DarkOrange;
+                        tolerance = 0.05m;
+                        break;
+                    case 6:
+                        pnlFourthBand.BackColor = Color.Yellow;
+                        tolerance = 0.02m;
+                        break;
+                    case 7:
+                        pnlFourthBand.BackColor = Color.Green;
+                        tolerance = 0.5m;
+                        break;
+                    case 8:
+                        pnlFourthBand.BackColor = Color.Blue;
+                        tolerance = 0.25m;
+                        break;
+                    case 9:
+                        pnlFourthBand.BackColor = Color.DarkViolet;
+                        tolerance = 0.1m;
+                        break;
+                    case 10:
+                        pnlFourthBand.BackColor = Color.LightGray;
+                        tolerance = 0.01m;
+                        break;
+                    default:
+                        break;
+
+                }
+            }
+
+            band4Set = true;
+            if (band1Set == true && band2Set == true &&
+                band3Set == true && band4Set == true)
+            {
+                btnProceed.Enabled = true;
+            }
+
+            lblResistanceValue.Text = String.Empty;
+
+        } // koniec kombo czwartego paska
+
+
+        private void btnProceed_Click(object sender, EventArgs e)
+        {
+            int number = firstNumber * 10 + secondNumber;
+            decimal resistanceValue = ((decimal)number * multiplier) / 1000;
+
+            lblResistanceValue.Text = resistanceValue.ToString() + "kΩ    " + "±" + tolerance + "%";
+
+
+        }
+
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            lblResistanceValue.Text = String.Empty;
+            comboFirstBand.ResetText();
+            comboSecondBand.ResetText();
+            comboThirdBand.ResetText();
+            comboFourthBand.ResetText();
+            band1Set = false;
+            band2Set = false;
+            band3Set = false;
+            band4Set = false;
+            btnProceed.Enabled = false;
+            pnlFirstBand.BackColor = Color.White;
+            pnlSecondBand.BackColor = Color.White;
+            pnlThirdBand.BackColor = Color.White;
+            pnlFourthBand.BackColor = Color.White;
+        }
+
+    } 
+    
+}
